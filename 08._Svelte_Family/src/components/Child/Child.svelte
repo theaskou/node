aed989d3c889aaa4dafbc349954c8c5dcd8b0e36
thea <thsk0001@stud.kea.dk>
2026-03-19 10:56:26 +0100

    sending props to parent component example

--- 08._Svelte_Family/src/components/Child/Child.svelte
+++ 08._Svelte_Family/src/components/Child/Child.svelte
@@ -1,5 +1,12 @@
 <script>
-    let { name, familySheep, isGirl } = $props();
+    let {
+        name,
+        familySheep,
+        isGirl,
+        onShowLove,
+        onEatCookie,
+        drinkFridge = $bindable()
+    } = $props();
 </script>
 
 <div
@@ -8,6 +15,10 @@
     class:is-boy={!isGirl}
 >
     <h3>I'm just child known as {name}</h3>
+    <button onclick={() => onShowLove(name)}>Show love 💕</button>
+    <button onclick={onEatCookie}>Eat cookie</button>
+    <button onclick={() => drinkFridge.pop()}>Have a drink</button>
+
 </div>
 
 <style>
--- 08._Svelte_Family/src/components/Parent/Parent.svelte
+++ 08._Svelte_Family/src/components/Parent/Parent.svelte
@@ -2,12 +2,29 @@
     import Child from "../Child/Child.svelte";
 
     let { name, children } = $props();
+
+    function handleShowLove(name) {
+        console.log(`${name} loves you <3`)
+    }
+
+    let cookieJar = $state(['🍪', '🍪', '🍪', '🍪']);
+
+    let drinkFridge = $state(['🧃','🥛','🧃','🧃']);
+
+    function handleEatCookie() {
+        cookieJar.pop();
+    }
+
 </script>
 
 <h1>My name is {name}.</h1>
+<p>{cookieJar}</p>
+<p>{drinkFridge}</p>
 
 {#each children as child (child.name)}
 
-    <Child {...child}/>
+    <Child {...child} onShowLove={handleShowLove} onEatCookie={handleEatCookie} drinkFridge={drinkFridge} />
 
 {/each}
+
+
